82b0a1650920d7d2f5025d2683c98830e42aeba6
shillwil <alex.shillingford@icloud.com>
2019-06-04 10:20:45 -0600

    Initial Commit

---
+++ About Me iOS Seven/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me iOS Seven
+//
+//  Created by Alex Shillingford on 6/4/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
